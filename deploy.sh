8143681da5077af2e751215b7208997a1cc0b8ea
Kindred <KDSM@protonmail.com>
2022-07-20 22:08:45 +0100

    init commit

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+
+set -e
+
+
+npm run build
+
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+
+ git push -f git@github.com:kindredsm/vue-todo-app.git master:gh-pages
+
+cd -
